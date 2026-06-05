3d717857e87d73d8091ae12f3dad45e05c29020d
deepika30gowda-bca <deepika30gowda@gmail.com>
2026-06-05 17:58:00 +0530

    Create Even_odd.java

---
+++ Even_odd.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+public class EvenOdd
+{
+    public static voidmain (String args[])
+    {
+        Scanner reader = new Scanner (System.in);
+        System.out.println("Enter a number : ");
+        int num = reader.nextInt();
+        if (num % 2 == 0)
+            System.out.println("num + " is even ");
+        else
+            System.out.println(num + " is odd");
+    }
+}
