7e19ae72a49afd800ef3512358ed02c4c451675a
deepika30gowda-bca <deepika30gowda@gmail.com>
2026-06-05 17:59:00 +0530

    Create Size_of_datatypes.java

---
+++ Size_of_datatypes.java
@@ -0,0 +1,15 @@
+class size_of_datatypes
+{
+    public static voidmain (String args[])
+    {
+        System.out.println("Hello world");
+        System.out.println("S.No \t datatype \t SIZE");
+        System.out.println("1 \t \t Byte \t \t \t" + Byte.SIZE);
+        System.out.println("2 \t \t Short \t \t \t" + Short.SIZE);
+        System.out.println("3 \t \t Integer \t \t \t" + Integer.SIZE);
+        System.out.println("4 \t \t Float \t \t \t" + Float.SIZE);
+        System.out.println("5 \t \t Long \t \t \t" + Long.SIZE);
+        System.out.println("6 \t \t Double \t \t" + Double.SIZE);
+        System.out.println("7 \t \t Character \t \t" + Character.SIZE);
+    }
+}
